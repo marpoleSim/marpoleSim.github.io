21300f00ccc1222e377760cb802779ec335d2aa8
Charles L <charles.li@marpolesim.com>
2026-02-18 16:51:48 -0800

    fix cover page

--- js/coverPageScript.js
+++ js/coverPageScript.js
@@ -1,11 +1,12 @@
 // this will call the function which is not expected
 // window.onload = choosePic;
 
-var myPix = new Array("a2b.jpg","a3b.jpg","a4b.jpg","a5b.jpg");
+var myPix = new Array("c1.jpg", "a2b.jpg","a3b.jpg","a4b.jpg","a5b.jpg");
 
 function choosePic() {
      // Get a random index
-     var randomNum = Math.floor(Math.random() * myPix.length);
+     //var randomNum = Math.floor(Math.random() * myPix.length);
+     var randomNum = 0
 
      // Get an image at the random_index
      selected_image = myPix[randomNum];
